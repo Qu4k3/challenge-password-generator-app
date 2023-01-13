73c164d940095f1c6055560d3a7b3d62f18dc402
Qu4k3 <paul.guillamon@gmail.com>
2023-01-13 20:05:15 +0100

    Skeletton

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { FaRegCopy, FaArrowRight } from "react-icons/fa";
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="password-generator">
+      <div className="wrapper">
+        <h1>Password Generator</h1>
+        <div className="block">
+          <input
+            className=""
+            type="text"
+            placeholder="P4$5W0rD!"
+          />
+          <FaRegCopy />
+        </div>
+        <div className="block generator">
+          <div className="generator__length">
+            <div>
+              <h4>Character Length</h4>
+            </div>
+            <span>10</span>
+          </div>
+          <input type="range" min="0" max="20" step="1" />
+          <div className="generator__checkbox-wrapper">
+            <label className="checkbox-wrapper">
+              <input
+                type="checkbox"
+                id="cbox1"
+                value="first_checkbox"
+              /><span class="checkmark"></span> Include Uppercase Letters
+            </label>
+            <label className="checkbox-wrapper">
+              <input
+                type="checkbox"
+                id="cbox1"
+                value="first_checkbox"
+              /><span class="checkmark"></span> Include Lowercase Letters
+            </label>
+            <label className="checkbox-wrapper">
+              <input
+                type="checkbox"
+                id="cbox1"
+                value="first_checkbox"
+              /><span class="checkmark"></span> Include Numbers
+            </label>
+            <label className="checkbox-wrapper">
+              <input
+                type="checkbox"
+                id="cbox1"
+                value="first_checkbox"
+              /><span class="checkmark"></span> Include Symbols
+            </label>
+          </div>
+          <div className="generator__strength-meter">
+            <div>Strength</div>
+            <ul>
+              <span>MEDIUM</span>
+              <li className="poor"></li>
+              <li className="low"></li>
+              <li className="medium"></li>
+              <li className="good"></li>
+            </ul>
+          </div>
+          <button>
+            <span>Generate</span>
+            <div><FaArrowRight /></div>
+          </button>
+        </div>
+
+      </div>
+    </section>
   );
 }
 
