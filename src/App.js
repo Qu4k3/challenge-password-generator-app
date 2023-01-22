545cbf42a163b26bc73eff48850b3272ee3beae6
Qu4k3 <paul.guillamon@gmail.com>
2023-01-22 09:51:10 +0100

    ADD useState

--- src/App.js
+++ src/App.js
@@ -1,76 +1,94 @@
+import { useState } from "react";
 import { FaRegCopy, FaArrowRight } from "react-icons/fa";
 import './App.scss';
 
 function App() {
-  return (
-    <section className="password-generator">
-      <div className="wrapper">
-        <h1>Password Generator</h1>
-        <div className="block">
-          <input
-            className=""
-            type="text"
-            placeholder="P4$5W0rD!"
-          />
-          <FaRegCopy />
-        </div>
-        <div className="block generator">
-          <div className="generator__length">
-            <div>
-              <h4>Character Length</h4>
-            </div>
-            <span>10</span>
-          </div>
-          <input type="range" min="0" max="20" step="1" />
-          <div className="generator__checkbox-wrapper">
-            <label className="checkbox-wrapper">
-              <input
-                type="checkbox"
-                id="cbox1"
-                value="first_checkbox"
-              /><span class="checkmark"></span> Include Uppercase Letters
-            </label>
-            <label className="checkbox-wrapper">
-              <input
-                type="checkbox"
-                id="cbox1"
-                value="first_checkbox"
-              /><span class="checkmark"></span> Include Lowercase Letters
-            </label>
-            <label className="checkbox-wrapper">
-              <input
-                type="checkbox"
-                id="cbox1"
-                value="first_checkbox"
-              /><span class="checkmark"></span> Include Numbers
-            </label>
-            <label className="checkbox-wrapper">
-              <input
-                type="checkbox"
-                id="cbox1"
-                value="first_checkbox"
-              /><span class="checkmark"></span> Include Symbols
-            </label>
-          </div>
-          <div className="generator__strength-meter">
-            <div>Strength</div>
-            <ul>
-              <span>MEDIUM</span>
-              <li className="poor"></li>
-              <li className="low"></li>
-              <li className="medium"></li>
-              <li className="good"></li>
-            </ul>
-          </div>
-          <button>
-            <span>Generate</span>
-            <div><FaArrowRight /></div>
-          </button>
-        </div>
 
+  const [password, setPassword] = useState("")
+  const [charLength, setCharLength] = useState(0)
+
+  const checkboxes = [
+    {
+      "id": "uppercase_letters",
+      "name": "Include Uppercase Letters"
+    }, {
+      "id": "lower_letters",
+      "name": "Include Lowercase Letters"
+    },
+    {
+      "id": "numbers",
+      "name": "Include Numbers"
+    },
+    {
+      "id": "symbols",
+      "name": "Include Symbols"
+    }
+  ]
+
+
+return (
+  <section className="password-generator">
+    <div className="wrapper">
+      <h1>Password Generator</h1>
+      <div className="block">
+        <input
+          className="password"
+          type="text"
+          placeholder="P4$5W0rD!"
+          value={password}
+          readonly
+        />
+        <FaRegCopy className="copy" />
       </div>
-    </section>
-  );
+      <div className="block generator">
+        <div className="generator__length">
+          <div>
+            <h4>Character Length</h4>
+          </div>
+          <span>10</span>
+        </div>
+        <input
+          type="range"
+          min="0"
+          max="20"
+          step="1"
+          defaultValue={charLength}
+          onChange={(event) => { setCharLength(event.target.value) }}
+        />
+        <div className="generator__checkbox-wrapper">
+            {
+              checkboxes.map((checkbox) => {
+                return (
+                <label className="checkbox-wrapper">
+                  <input
+                    type="checkbox"
+                    id={checkbox.id}
+                    onChange={(event) => { console.log(event.target.value) }}
+                  /><span class="checkmark"></span> {checkbox.name}
+                </label>
+                )
+              })
+            }
+        </div>
+        <div className="generator__strength-meter">
+          <div>Strength</div>
+          <ul>
+            <span>MEDIUM</span>
+            <li className="poor"></li>
+            <li className="low"></li>
+            <li className="medium"></li>
+            <li className="good"></li>
+          </ul>
+        </div>
+        <button>
+          <span>Generate</span>
+          <div><FaArrowRight /></div>
+        </button>
+      </div>
+
+    </div>
+  </section>
+);
 }
 
 export default App;
